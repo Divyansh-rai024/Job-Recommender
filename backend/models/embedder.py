06d41b2eaff7f620283fee47a3892bc21b5c89fe
Divyansh-rai024 <divyanshrai6307@gmail.com>
2025-04-07 12:15:18 +0530

    embedder.py

--- backend/models/embedder.py
+++ backend/models/embedder.py
@@ -1,4 +1,9 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 def get_model():
     return SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed_text(model, text):
+    embedding = model.encode([text])[0]
+    return np.array(embedding)
